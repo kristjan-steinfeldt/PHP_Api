f372c78ca3a3308839e44c15e6482bfaac956d1e
kristjan-steinfeldt <98741231+kristjan-steinfeldt@users.noreply.github.com>
2023-01-25 20:59:28 +0200

    Final index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 include "Database.php";
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=utf-8");
@@ -7,32 +7,53 @@ header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
 
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uriData = explode( '/', $uri );
+$uriData = explode('/', $uri);
 $requestBodyText = file_get_contents('php://input');
 $requestBody = json_decode($requestBodyText);
-
-function append_to_file($line_to_append) {
+$conn = mysqli_connect($servername, $username, $password, $db) or die("Connect failed: %s\n" . $conn->error);
+function append_to_file($line_to_append)
+{
   $date = date('Y/m/d H:i:s');
   $file_path = dirname(__FILE__) . '/logs.txt';
   $file = fopen($file_path, "a");
-  fwrite($file, $line_to_append . ",'$date' \n");
+  fwrite($file, $line_to_append . ", '$date' \n");
   fclose($file);
 }
-switch ($method) {
-    case 'PUT':
+function escape($data)
+{
+  return $data = str_replace(',', '\,', $data);
+}
 
-      $name =$requestBody->name;
-      $id=$requestBody->id;
-      // $name="updatedname";
-      // $id="1";
-      $conn = mysqli_connect($servername, $username, $password, $db) or die("Connect failed: %s\n". $conn -> error);
-      
+function check_session($session)
+{
+  global $conn;
+  $sql = "SELECT * FROM sessions WHERE id='$session';";
+  $result = mysqli_query($conn, $sql);
+  $test = mysqli_fetch_assoc($result);
+  if ($test['id'] == $session) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+
+
+switch ($method) {
+  case 'PUT':
+
+    $session = $requestBody->session;
+    $name = $requestBody->name;
+    $id = $requestBody->id;
+
+    if (check_session($session)) {
       $stmt = mysqli_prepare($conn, "UPDATE playlists SET name = ? WHERE id = ?");
       mysqli_stmt_bind_param($stmt, "si", $name, $id);
-      
+
       if (mysqli_stmt_execute($stmt)) {
         echo "Playlist $id, $name updated successfully.";
-        append_to_file("PUT, $id, $name");
+        append_to_file("PUT, $id, " . escape($name));
       } else {
         echo "Error updating playlist: " . mysqli_error($conn);
       }
@@ -40,46 +61,96 @@ switch ($method) {
       mysqli_stmt_close($stmt);
       mysqli_close($conn);
       break;
-    case 'POST':
-
-      $name =$requestBody->name;
-      $userid =$requestBody->userid;
-      $conn = mysqli_connect($servername, $username, $password,$db) or die("Connect failed: %s\n". $conn -> error);
-      $sql = "INSERT INTO playlists (name,description,public,userid) VALUES ('$name', 'description','true','$userid');";
-        $result = mysqli_query($conn, $sql);
-     
-      echo "Created $name";
-      append_to_file("POST, $name, $userid");
+    } else {
+      http_response_code(401);
+      echo json_encode(
+        array('message' => 'Invalid session')
+      );
       break;
-    case 'GET':
-      $userid=$uriData[2];
-      $conn = mysqli_connect($servername, $username, $password,$db) or die("Connect failed: %s\n". $conn -> error);
-      $sql = "SELECT id ,name FROM playlists WHERE userid='$userid'";
+    }
+  case 'POST':
+    if ($uriData[2] == 'sessions') {
+      $username = $requestBody->username;
+      $password = $requestBody->password;
+      $sql = "SELECT id FROM users WHERE usename = '$username' AND password = '$password';";
+      $users = mysqli_query($conn, $sql);
+      $user = mysqli_fetch_assoc($users);
+      if (count($user) === 0) {
+        http_response_code(401);
+        echo json_encode(
+          array('message' => 'Invalid username or password')
+        );
+        exit();
+      }
+
+      // Create session
+
+      $userid = $user['id'][0];
+      $bytes = random_bytes(16);
+      $sessionId = bin2hex($bytes);
+      $sql = "INSERT INTO sessions (id,userid) VALUES ('$sessionId','$userid');";
+      $insert = mysqli_query($conn, $sql);
+      $sql2 = "SELECT * FROM sessions WHERE id='$sessionId';";
+
+      $session = mysqli_query($conn, $sql2);
+      $thisSession = mysqli_fetch_assoc($session);
+      // Return session
+      echo json_encode(
+        array('sessionId' => $thisSession['id'], 'user' => $userid)
+      );
+    } else {
+
+      $name = $requestBody->name;
+      $userid = $requestBody->userid;
+      $session = $requestBody->session;
+      if (check_session($session)) {
+        $sql = "INSERT INTO playlists (name,description,public,userid) VALUES ('$name', 'description','true','$userid');";
+        echo "Created $name";
         $result = mysqli_query($conn, $sql);
 
-        $userArr = array();
-        while ($row = mysqli_fetch_assoc($result))
-        {
-            $userArr[] = $row;
-        }
-        $data =array("items" => $userArr);
-    
-        echo json_encode($data,JSON_UNESCAPED_SLASHES);
-      
-      break;
-    case strtoupper('delete'):
-          $id=$uriData[2];
-          $sql = "DELETE FROM playlists WHERE id = $id;";
-          $conn = mysqli_connect($servername, $username, $password,$db) or die("Connect failed: ". $conn -> error);
-          $result = mysqli_query($conn, $sql);
-          append_to_file("DELETE,$id");
-
+        append_to_file("POST, $userid, " . escape($name));
         break;
-    default:
+      } else {
+        http_response_code(401);
+        echo json_encode(
+          array('message' => 'Invalid session')
+        );
+      }
+    }
+  case 'GET':
+    $userid = $uriData[2];
+    $sql = "SELECT id ,name FROM playlists WHERE userid='$userid'";
+    $result = mysqli_query($conn, $sql);
+
+    $userArr = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+      $userArr[] = $row;
+    }
+    $data = array("items" => $userArr);
+
+    echo json_encode($data, JSON_UNESCAPED_SLASHES);
+    break;
+  case strtoupper('delete'):
+    $session = $requestBody->session;
+    if (check_session($session)) {
+      $id = $uriData[2];
+      $sql = "DELETE FROM playlists WHERE id = $id;";
+      $result = mysqli_query($conn, $sql);
+      append_to_file("DELETE,$id");
+      echo "playlist $id deleted";
+    } else {
+      http_response_code(401);
+      echo json_encode(
+        array('message' => 'Invalid session')
+      );
+    }
+
+    break;
+  default:
     echo json_encode(
-        array('message' => 'method unknown')
+      array('message' => 'method unknown')
     );
-      break;
-  }
+    break;
+}
 
 ?>
